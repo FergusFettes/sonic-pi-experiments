02438eabdbf98624969b0597634d17b7baba6458
fergus <fergusfettes@gmail.com>
2020-09-20 23:52:47 +0100

    tring out operations in time

--- test.rb
+++ test.rb
@@ -1,19 +1,28 @@
 play 70
-sleep 1
+
+sleep 0.1
+
 play 70
 play 75
-sleep 1
+
+sleep 0.001
+
 play 76
 play 77
 play 80
+
 sleep 1
+
 play 81
 play 82
 play 85
 play 90
-sleep 1
+
+sleep (2/1)
+
 play 91
 play 92
+
 # live_loop :flibble do
 #   sample :bd_haus, rate: 1
 #   sleep 0.5

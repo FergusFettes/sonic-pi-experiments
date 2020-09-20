11058bc3463b8cd6b41eb9e27b2773f172018fda
fergus <fergusfettes@gmail.com>
2020-09-20 23:45:56 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sonic Pi Experiments
+
+I'm going to start exploring Sonic Pi. This is where I will record my progress.
---
+++ test.rb
@@ -0,0 +1,20 @@
+play 70
+sleep 1
+play 70
+play 75
+sleep 1
+play 76
+play 77
+play 80
+sleep 1
+play 81
+play 82
+play 85
+play 90
+sleep 1
+play 91
+play 92
+# live_loop :flibble do
+#   sample :bd_haus, rate: 1
+#   sleep 0.5
+# end

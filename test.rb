6e5db827e5f0f232abf1b3ff1da36145c1dac120
fergus <fergusfettes@gmail.com>
2020-09-21 20:39:40 +0100

    more rates

--- test.rb
+++ test.rb
@@ -1,15 +1,38 @@
- use_synth :prophet
+# in_thread do
+#   loop do
+#     sample :ambi_drone, rate: 0.5
+#     sample :ambi_drone, rate: 1
+#     sleep sample_duration :ambi_drone, rate: 0.5
+#   end
+# end
 
- in_thread do
-   loop do
-     play 65, attack: 0.5, sustain: 2, release: 1, amp: 0.4
-     play 65, attack: 0.5, sustain: 2, release: 1, amp: 0.2
-     play 70, attack: 0.5, release: 3
-     play 72, attack: 0.1, release: 2
-     play 73, attack: 4, release: 0.2, amp: 0.4
-     sleep 4
-   end
+in_thread do
+  loop do
+    sample :loop_amen, rate: -0.5
+    sample :loop_amen, rate: 0.5
+    sleep sample_duration :loop_amen, rate: -0.5
+  end
 end
+
+use_synth :prophet
+
+ # in_thread do
+ #   loop do
+ #     play 65, attack: 0.5, attack_level: 1, decay: 0.5, sustain_level: 0.4, sustain: 2, release: 1, amp: 1
+ #     if one_in(5)
+ #       play 65, attack: 0.5, sustain: 2, release: 1, amp: 0.4
+ #       play 65, attack: 0.5, sustain: 2, release: 1, amp: 0.2
+ #     end
+ #     if one_in(7)
+ #       play 70, attack: 0.5, release: 3
+ #       play 72, attack: 0.1, release: 2
+ #     end
+ #     if one_in(2)
+ #       play 73, attack: 4, release: 0.2, amp: 0.4
+ #     end
+ #     sleep 4
+ #   end
+# end
 
 
 # in_thread(name: :main) do
@@ -31,13 +54,6 @@ end
 #       sample :drum_tom_mid_soft
 #       sleep 0.125
 #     end
-#   end
-# end
-
-# in_thread do
-#   loop do
-#     sample :loop_amen
-#     sleep sample_duration :loop_amen
 #   end
 # end
 
